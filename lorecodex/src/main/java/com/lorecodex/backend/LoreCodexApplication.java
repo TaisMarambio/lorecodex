964ceadd91b21c52a7ee38d4fcc0000d5a568f45
taismmc <tmmarambiocapelli@mail.austral.edu.ar>
2025-03-28 00:18:03 -0300

    Initial commit

---
+++ lorecodex/src/main/java/com/lorecodex/backend/LoreCodexApplication.java
@@ -0,0 +1,13 @@
+package com.lorecodex.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoreCodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoreCodexApplication.class, args);
+	}
+
+}
---
+++ lorecodex/src/test/java/com/lorecodex/backend/LoreCodexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorecodex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoreCodexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
