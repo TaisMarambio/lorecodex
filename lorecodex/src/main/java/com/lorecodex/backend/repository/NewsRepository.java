1cf2f89f22af234d5ecfca7a5e11b006d9fc2aa5
taismmc <tmmarambiocapelli@mail.austral.edu.ar>
2025-05-26 01:10:04 -0300

    Remove unused imports from NewsRepository
    Removed unnecessary imports for Query and Param annotations. This change cleans up the code and eliminates unused dependencies to improve code readability.

--- lorecodex/src/main/java/com/lorecodex/backend/repository/NewsRepository.java
+++ lorecodex/src/main/java/com/lorecodex/backend/repository/NewsRepository.java
@@ -2,8 +2,6 @@ package com.lorecodex.backend.repository;
 
 import com.lorecodex.backend.model.News;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
